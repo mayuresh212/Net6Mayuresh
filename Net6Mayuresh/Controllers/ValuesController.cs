5c9baaa333b564a2fbd0b18c28a02922352ba6f3
mayuresh212 <mayuresh.212@gmail.com>
2022-04-29 21:56:09 +0530

    Add project files.

---
+++ Net6Mayuresh/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Net6Mayuresh.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "FirstApi")]
+        public string Get()
+        {
+            return "FirstApi .net 6";
+        }
+    }
+}
